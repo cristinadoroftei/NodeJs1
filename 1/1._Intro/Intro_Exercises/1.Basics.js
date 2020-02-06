32ea6a8c442e405f8ec27c148e0cc28433c86a85
CristinaDoroftei <dorofteicristina19@gmail.com>
2020-02-06 09:51:44 +0100

    Added the first exercises

---
+++ 1/1._Intro/Intro_Exercises/1.Basics.js
@@ -0,0 +1,10 @@
+//ex1
+var firstName = "Anders";
+var lastName = "Latif"
+console.log(`My first name is ${firstName} and my last name is ${lastName}`)
+
+//ex2
+var year = "2017"
+var number = 1;
+var newYear = parseInt(year) + number
+console.log(newYear)
